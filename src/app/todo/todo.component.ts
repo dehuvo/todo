001bacbd6a803e8868f2a27e38ae70352cc0714b
Huang Degil <huang.degil@gmail.com>
2018-10-02 23:02:22 +0900

    Simplified

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -9,101 +9,75 @@ import { Todo } from './todo';
   template: `
   <div class="container">
   <h3>To-Do List</h3>
-  <form #f="ngForm" (ngSubmit)="add(f)">
+  <form #f="ngForm" (ngSubmit)="add(f)" class="form-inline">
     <label>새 항목: &nbsp;
-      <input type="text" name="title" [ngModel]="f.value.title" required>
+      <input type="text" name="title" [ngModel]="f.value.title"
+             required pattern="\\s*\\S.*" class="form-control">
     </label>
-    <button type="submit" [disabled]="f.invalid">추가</button>
+    <button type="submit" [disabled]="f.invalid" class="btn">
+      <i class="fas fa-plus-square fa-lg"></i>
+    </button>
   </form>
   <br>
   <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>No</th>
-        <th>Title</th>
-        <th>Menu</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr *ngFor="let todo of todos">
-        <td>{{todo.id}}</td>
-        <td class="{{todo.done? 'done': ''}}">{{todo.title}}</td>
-        <td>
-          <button type="button" (click)="setDone($event)">완료</button>
-          <button type="button" (click)="remove($event)">삭제</button>
-        </td>
-      </tr>
-    </tbody>
+    <tr>
+      <th>No</th>
+      <th>Title</th>
+      <th>Menu</th>
+    </tr>
+    <tr *ngFor="let todo of todos">
+      <td>{{todo.id}}</td>
+      <td class="{{todo.done? 'done': ''}}">{{todo.title}}</td>
+      <td>
+        <button type="button" (click)="setDone(todo)">
+          <i class="fas fa-check-square fa-lg"></i>
+        </button>
+        <button type="button" (click)="remove(todo.id)">
+          <i class="fas fa-minus-square fa-lg"></i>
+        </button>
+      </td>
+    </tr>
   </table>
 </div>
-`,
+` ,
   styles: [`
   .done {
     text-decoration: line-through;
   }
-`]
+` ]
 })
 export class TodoComponent implements OnInit {
   todos: Todo[];
-  errorMessage: string;
 
   constructor(private todoHttpService: TodoHttpService) { }
 
   ngOnInit() {
-    this.get();
+    this.todoHttpService.get().subscribe(todos => this.todos = todos);
   }
 
   add(form: NgForm) {
-    if (form.valid) {
-      const todo = Object.assign({ done: false }, form.value);
-      this.todoHttpService.add(todo).subscribe(
-        id => {
-          todo.id = id;
-          this.todos.push(todo);
-          form.reset();
-        },
-        error => this.errorMessage = error
-      );
-    }
-  }
-
-  get() {
-    this.todoHttpService.get().subscribe(
-      todos => this.todos = todos,
-      error => this.errorMessage = error
+    const todo = Object.assign({ done: false }, form.value);
+    todo.title = todo.title.trim();
+    this.todoHttpService.add(todo).subscribe(
+      id => {
+        todo.id = id;
+        this.todos.push(todo);
+        form.reset();
+      }
     );
-  }
-
-  getId(event: any) {
-    return parseInt(event.target.parentNode.parentNode.cells[0].textContent);
   }
   
-  setDone(event: any) {
-    const id = this.getId(event);
-    const todo = this.todos.find(o => o.id === id);
+  setDone(todo: Todo) {
     todo.done = !todo.done;
-    this.todoHttpService.update(todo).subscribe(
-      () => {
-        const classList = event.target.parentNode.parentNode.cells[1].classList;
-        if (todo.done) {
-          classList.add('done');
-        } else {
-          classList.remove('done');
-        }
-      },
-      error => this.errorMessage = error
-    );
+    this.todoHttpService.update(todo);
   }
 
-  remove(event: any) {
-    const id = this.getId(event);
+  remove(id: number) {
     this.todoHttpService.remove(id).subscribe(
       () => {
-        const index = this.todos.findIndex(data => data.id === id);
+        const index = this.todos.findIndex(todo => todo.id === id);
         this.todos.splice(index, 1);
-      },
-      error => this.errorMessage = error
+      }
     );
-    return false;
   }
 }
