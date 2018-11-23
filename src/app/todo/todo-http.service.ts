82bf07de70e64f87988b0ca5bc80b6d71b6570ae
Huang Degil <huang.degil@gmail.com>
2018-11-23 16:37:51 +0900

    Added error processing

--- src/app/todo/todo-http.service.ts
+++ src/app/todo/todo-http.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { Todo } from './todo';
 
-const URL = 'http://localhost:3000/data';
+const URL = 'http://localhost:3000/data/';
 
 const HTTP_OPTIONS = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -20,14 +21,28 @@ export class TodoHttpService {
   }
 
   add(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>(URL, todo, HTTP_OPTIONS);
+    return this.http.post<Todo>(URL, todo, HTTP_OPTIONS).pipe(
+      catchError(this.handleError<any>('add'))
+    );
   }
 
   update(todo: Todo): Observable<any> {
-    return this.http.patch<Todo>(URL + '/' + todo.id, todo, HTTP_OPTIONS);
+    return this.http.patch<Todo>(URL + todo.id, todo, HTTP_OPTIONS).pipe(
+      catchError(this.handleError<any>('update'))
+    );
   }
 
   remove(id: number): Observable<any> {
-    return this.http.delete(URL + '/' + id);
+    return this.http.delete(URL + id).pipe(
+      catchError(this.handleError<any>('delete'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      console.log(`${operation} failed: ${error.message}`);
+      return of(result as T);
+    };
   }
 }
